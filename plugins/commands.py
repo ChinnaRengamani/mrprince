aa1099752646a4ee7b1758ce0dcee03c0c99b5ab
Rengamani3 <127839457+Rengamani3@users.noreply.github.com>
2023-05-14 21:01:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -140,7 +140,7 @@ async def start(client, message):
                 file_id=file.file_id,
                 caption=f_caption,
                 protect_content=True if pre == 'filep' else False,
-                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/piroxbots") ] ] ),
+                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ Tᴇᴀᴍ Mʀ Pʀɪɴᴄᴇ ⚔️', url="https://t.me/teammrprince") ] ] ),
             )
         return
     
